71ffd45e556223dad3bcf1c697311646a6b08715
Victor Valotto <vvalotto@gmail.com>
2020-02-23 11:22:14 -0300

    Pruebas para distribuir app

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ if __name__ == "__main__":
     """
     Aplicacion de servicios corre en el servidor definido (sin direccion IP especifica)
     """
-    app_api.run(host='0.0.0.0', port=7000, debug=True, ssl_context=("2018-11_energiaer.com.ar.crt", "www.energiaer.com.ar.key"))
+    app_api.run(host='0.0.0.0', port=5000, debug=True,
+                ssl_context=("2018-11_energiaer.com.ar.crt", "www.energiaer.com.ar.key"))
--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
-from setuptools import  setup, find_packages
+from setuptools import setup, find_packages
 
 
 setup(
     name="errores_fortinet",
     version="1.0.0",
+    description="Servicios Web para registrar los errores 504 presentados en los navegadores",
+
+    author="Victor Valotto",
+    author_email="vvalotto@enersa.com.ar",
+
     packages=find_packages(),
+    py_modules=['app'],
+    include_package_data=True,
+    install_requires=['Flask>=1.1.1',
+                      'Flask-Cors>=3.0.8',
+                      'pyOpenSSL>=19.1.0'],
 )
